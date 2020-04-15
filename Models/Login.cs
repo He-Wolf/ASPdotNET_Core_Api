2ecc1a48b960d89a94de6178ed90a92f92a6d674
László Ceglédi <Laszlo.Cegledi@heitec.hu>
2020-04-15 11:46:11 +0200

    accountcontroller added

--- Models/Login.cs
+++ Models/Login.cs
@@ -7,6 +7,7 @@ namespace TodoApi.Models
         [Required(ErrorMessage = "Email address is required")]
         [EmailAddress]
         public string Email { get; set; }
+        
         [Required(ErrorMessage = "Password is required")]
         [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
         [DataType(DataType.Password)]
